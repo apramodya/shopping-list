790910a2c2724b028a45c2597cb0d95b91e63572
apramodya <pramodyaabeysinghe@gmail.com>
2018-04-28 02:17:56 +0530

    Update entry.js

--- entry.js
+++ entry.js
@@ -6,9 +6,7 @@ const path = require('path');
 const routes = require('./routes/routes');
 var app = express();
 
-mongoose.connect('mongodb://admin:admin123@ds161459.mlab.com:61459/shoppinglist',{
-    useMongoClient: true
-});
+mongoose.connect('mongodb://admin:admin123@ds161459.mlab.com:61459/shoppinglist');
 mongoose.connection.on('connected', () => {
     console.log('db connected');
 });
@@ -26,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) =>{
    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
+app.get('/', (req, res)=> {
+    res.send('ok');
+})
 
 const port = process.env.port || 8080;
 app.listen(port, () => {
